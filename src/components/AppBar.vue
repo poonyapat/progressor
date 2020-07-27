3da589e56134eebfba1480e53e03c8732fc23b45
Poon <deity_ngo@hotmail.com>
2020-07-28 01:37:51 +0700

    add sign in and remove unneccessary code

---
+++ src/components/AppBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <span v-if="googleUser"> {{googleUser['Ot']['Cd']}} </span>
+      <v-spacer></v-spacer>
+      <v-btn @click="signIn" text>Sign In</v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+import {mapState, mapMutations} from 'vuex'
+export default {
+  methods: {
+    async signIn() {
+      const googleUser = await this.$gAuth.signIn()
+      this.updateUser(googleUser)
+    },
+    ...mapMutations(['updateUser'])
+  },
+  computed: {
+  ...mapState({
+    googleUser: state => state.user
+  })
+},
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,27 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import GAuth from 'vue-google-oauth2'
 import '@babel/polyfill'
 
 Vue.config.productionTip = false
+
+const gauthOption = {
+  clientId: '27339545554-qi7vnrp523n3kq99qllurm6rrj3ikjlc.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'select_account'
+}
+Vue.use(GAuth, gauthOption)
+
+Vue.mixin({
+  data: function() {
+    return {
+      get projectName() {
+        return "Progreaxaz"
+      }
+    }
+  }
+})
 
 new Vue({
   router,
